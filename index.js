f00bd55a1543b0e79165b012cce2f912f5beb608
sourav-xe <sourav20kumar02@gmail.com>
2025-03-04 16:57:14 +0530

    Portfolio

---
+++ index.js
@@ -0,0 +1,7 @@
+let currentPage = window.location.pathname.split("/").pop();
+
+document.querySelectorAll("nav a").forEach(link=>{
+    if(link.getAttribute("href") === currentPage){
+        link.classList.add("active");
+    }
+});
