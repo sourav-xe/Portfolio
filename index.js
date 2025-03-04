65b3abff271fc0ac8cced3bcd5e69871f89bbf18
sourav-xe <sourav20kumar02@gmail.com>
2025-03-05 00:15:30 +0530

    done

--- index.js
+++
@@ -1,7 +0,0 @@
-let currentPage = window.location.pathname.split("/").pop();
-
-document.querySelectorAll("nav a").forEach(link=>{
-    if(link.getAttribute("href") === currentPage){
-        link.classList.add("active");
-    }
-});
